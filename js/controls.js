13de781712acb3d77ee2f38420f63c35fc63be68
Innocent Igwonobe <innocentigwonobe@gmail.com>
2019-03-08 00:31:43 +0100

    add responsive to UI

--- js/controls.js
+++ js/controls.js
@@ -1,4 +1,5 @@
-// home page controls
+// index page controls
+
 function createAccount(){
 	document.getElementById('signup-box').style.display ="block";
 	document.getElementById('login-box').style.display ="none";
@@ -8,12 +9,11 @@ function loginIn(){
 	document.getElementById('login-box').style.display ="block";
 	document.getElementById('signup-box').style.display ="none";
 }
-// mobile Menu control
+
 function openToggleMenu(){
 	document.getElementById("toggle-menu").style.display="block";
 }
 
-// user dashboard control
 function openInbox(){
 	document.getElementById("inbox-box").style.display="block";
 	document.getElementById("send-box").style.display="none";
@@ -86,31 +86,4 @@ function openSettingBox(){
 
 function bcBtn(){
 	document.getElementById('bc-hidden').style.display="block";
-}
-
-function openDraftBox(){
-	document.getElementById("draft-box").style.display="block";
-	document.getElementById("send-box").style.display="none";
-	document.getElementById("inbox-box").style.display="none";
-	document.getElementById("compose-box").style.display="none";
-	document.getElementById("delete-box").style.display="none";
-	document.getElementById("spam-box").style.display="none";
-	document.getElementById("setting-box").style.display="none";
-}
-// Admin dashboard control
-function createGroup(){
-    document.getElementById('create-group-box').style.display="block";
-    document.getElementById('delete-group-box').style.display="none";
-    document.getElementById('all-group-box').style.display="none";
-}
-
-function deleteGroup(){
-    document.getElementById('delete-group-box').style.display="block";
-    document.getElementById('create-group-box').style.display="none";
-    document.getElementById('all-group-box').style.display="none";
-}
-function viewAllGroup(){
-    document.getElementById('all-group-box').style.display="block";
-    document.getElementById('delete-group-box').style.display="none";
-    document.getElementById('create-group-box').style.display="none";
 }
