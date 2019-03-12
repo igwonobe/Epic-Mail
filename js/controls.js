c3f650d954b34204d0bb5020890c51af9165010a
Innocent Igwonobe <innocentigwonobe@gmail.com>
2019-03-12 20:12:41 +0100

    Modify UI Design

--- js/controls.js
+++ js/controls.js
@@ -1,31 +1,31 @@
 // index page controls
 
-function createAccount(){
-	document.getElementById('signup-box').style.display ="block";
-	document.getElementById('login-box').style.display ="none";
+function createAccount() {
+	document.getElementById('signup-box').style.display = "block";
+	document.getElementById('login-box').style.display = "none";
 }
 
-function loginIn(){
-	document.getElementById('login-box').style.display ="block";
-	document.getElementById('signup-box').style.display ="none";
+function loginIn() {
+	document.getElementById('login-box').style.display = "block";
+	document.getElementById('signup-box').style.display = "none";
 }
 
-function openToggleMenu(){
-	document.getElementById("toggle-menu").style.display="block";
+function openToggleMenu() {
+	document.getElementById("toggle-menu").style.display = "block";
 }
-function closeToggleMenu(){
-	document.getElementById("toggle-menu").style.display="none";
+function closeToggleMenu() {
+	document.getElementById("toggle-menu").style.display = "none";
 }
 // dashboard Controls
-function openInbox(){
-	document.getElementById("inbox-box").style.display="block";
-	document.getElementById("send-box").style.display="none";
-	document.getElementById("draft-box").style.display="none";
-	document.getElementById("compose-box").style.display="none";
-	document.getElementById("delete-box").style.display="none";
-	document.getElementById("spam-box").style.display="none";
-	document.getElementById("setting-box").style.display="none";
-	document.getElementById("replyMsg").style.display="none";
+function openInbox() {
+	document.getElementById("inbox-box").style.display = "block";
+	document.getElementById("send-box").style.display = "none";
+	document.getElementById("draft-box").style.display = "none";
+	document.getElementById("compose-box").style.display = "none";
+	document.getElementById("delete-box").style.display = "none";
+	document.getElementById("spam-box").style.display = "none";
+	document.getElementById("setting-box").style.display = "none";
+	document.getElementById("replyMsg").style.display = "none";
 	document.getElementById("active2").style.backgroundColor = "#27a32d";
 	document.getElementById("active1").style.backgroundColor = "rgba(0,0,0,.9)";
 	document.getElementById("active3").style.backgroundColor = "rgba(0,0,0,.9)";
@@ -35,16 +35,16 @@ function openInbox(){
 	document.getElementById("active7").style.backgroundColor = "rgba(0,0,0,.9)";
 }
 
-function openSendBox(){
-	document.getElementById("send-box").style.display="block";
-	document.getElementById("inbox-box").style.display="none";
-	document.getElementById("draft-box").style.display="none";
-	document.getElementById("compose-box").style.display="none";
-	document.getElementById("delete-box").style.display="none";
-	document.getElementById("spam-box").style.display="none";
-	document.getElementById("setting-box").style.display="none";
-	document.getElementById("read-msg-box").style.display="none";
-	document.getElementById("replyMsg").style.display="none";
+function openSendBox() {
+	document.getElementById("send-box").style.display = "block";
+	document.getElementById("inbox-box").style.display = "none";
+	document.getElementById("draft-box").style.display = "none";
+	document.getElementById("compose-box").style.display = "none";
+	document.getElementById("delete-box").style.display = "none";
+	document.getElementById("spam-box").style.display = "none";
+	document.getElementById("setting-box").style.display = "none";
+	document.getElementById("read-msg-box").style.display = "none";
+	document.getElementById("replyMsg").style.display = "none";
 	document.getElementById("active3").style.backgroundColor = "#27a32d";
 	document.getElementById("active1").style.backgroundColor = "rgba(0,0,0,.9)";
 	document.getElementById("active2").style.backgroundColor = "rgba(0,0,0,.9)";
@@ -54,16 +54,16 @@ function openSendBox(){
 	document.getElementById("active7").style.backgroundColor = "rgba(0,0,0,.9)";
 }
 
-function openDraftBox(){
-	document.getElementById("draft-box").style.display="block";
-	document.getElementById("send-box").style.display="none";
-	document.getElementById("inbox-box").style.display="none";
-	document.getElementById("compose-box").style.display="none";
-	document.getElementById("delete-box").style.display="none";
-	document.getElementById("spam-box").style.display="none";
-	document.getElementById("setting-box").style.display="none";
-	document.getElementById("read-msg-box").style.display="none";
-	document.getElementById("replyMsg").style.display="none";
+function openDraftBox() {
+	document.getElementById("draft-box").style.display = "block";
+	document.getElementById("send-box").style.display = "none";
+	document.getElementById("inbox-box").style.display = "none";
+	document.getElementById("compose-box").style.display = "none";
+	document.getElementById("delete-box").style.display = "none";
+	document.getElementById("spam-box").style.display = "none";
+	document.getElementById("setting-box").style.display = "none";
+	document.getElementById("read-msg-box").style.display = "none";
+	document.getElementById("replyMsg").style.display = "none";
 	document.getElementById("active4").style.backgroundColor = "#27a32d";
 	document.getElementById("active1").style.backgroundColor = "rgba(0,0,0,.9)";
 	document.getElementById("active2").style.backgroundColor = "rgba(0,0,0,.9)";
@@ -73,16 +73,16 @@ function openDraftBox(){
 	document.getElementById("active7").style.backgroundColor = "rgba(0,0,0,.9)";
 }
 
-function openComposeBox(){
-	document.getElementById("compose-box").style.display="block";
-	document.getElementById("send-box").style.display="none";
-	document.getElementById("inbox-box").style.display="none";
-	document.getElementById("draft-box").style.display="none";
-	document.getElementById("delete-box").style.display="none";
-	document.getElementById("spam-box").style.display="none";
-	document.getElementById("setting-box").style.display="none";
-	document.getElementById("read-msg-box").style.display="none";
-	document.getElementById("replyMsg").style.display="none";
+function openComposeBox() {
+	document.getElementById("compose-box").style.display = "block";
+	document.getElementById("send-box").style.display = "none";
+	document.getElementById("inbox-box").style.display = "none";
+	document.getElementById("draft-box").style.display = "none";
+	document.getElementById("delete-box").style.display = "none";
+	document.getElementById("spam-box").style.display = "none";
+	document.getElementById("setting-box").style.display = "none";
+	document.getElementById("read-msg-box").style.display = "none";
+	document.getElementById("replyMsg").style.display = "none";
 	document.getElementById("active1").style.backgroundColor = "#27a32d";
 	document.getElementById("active2").style.backgroundColor = "rgba(0,0,0,.9)";
 	document.getElementById("active3").style.backgroundColor = "rgba(0,0,0,.9)";
@@ -92,16 +92,16 @@ function openComposeBox(){
 	document.getElementById("active7").style.backgroundColor = "rgba(0,0,0,.9)";
 }
 
-function openSpamBox(){
-	document.getElementById("spam-box").style.display="block";
-	document.getElementById("delete-box").style.display="none";
-	document.getElementById("send-box").style.display="none";
-	document.getElementById("inbox-box").style.display="none";
-	document.getElementById("draft-box").style.display="none";
-	document.getElementById("compose-box").style.display="none";
-	document.getElementById("setting-box").style.display="none";
-	document.getElementById("read-msg-box").style.display="none";
-	document.getElementById("replyMsg").style.display="none";
+function openSpamBox() {
+	document.getElementById("spam-box").style.display = "block";
+	document.getElementById("delete-box").style.display = "none";
+	document.getElementById("send-box").style.display = "none";
+	document.getElementById("inbox-box").style.display = "none";
+	document.getElementById("draft-box").style.display = "none";
+	document.getElementById("compose-box").style.display = "none";
+	document.getElementById("setting-box").style.display = "none";
+	document.getElementById("read-msg-box").style.display = "none";
+	document.getElementById("replyMsg").style.display = "none";
 	document.getElementById("active5").style.backgroundColor = "#27a32d";
 	document.getElementById("active1").style.backgroundColor = "rgba(0,0,0,.9)";
 	document.getElementById("active2").style.backgroundColor = "rgba(0,0,0,.9)";
@@ -112,16 +112,16 @@ function openSpamBox(){
 }
 
 
-function openDeleteBox(){
-	document.getElementById("delete-box").style.display="block";
-	document.getElementById("send-box").style.display="none";
-	document.getElementById("inbox-box").style.display="none";
-	document.getElementById("draft-box").style.display="none";
-	document.getElementById("compose-box").style.display="none";
-	document.getElementById("spam-box").style.display="none";
-	document.getElementById("setting-box").style.display="none";
-	document.getElementById("read-msg-box").style.display="none";
-	document.getElementById("replyMsg").style.display="none";
+function openDeleteBox() {
+	document.getElementById("delete-box").style.display = "block";
+	document.getElementById("send-box").style.display = "none";
+	document.getElementById("inbox-box").style.display = "none";
+	document.getElementById("draft-box").style.display = "none";
+	document.getElementById("compose-box").style.display = "none";
+	document.getElementById("spam-box").style.display = "none";
+	document.getElementById("setting-box").style.display = "none";
+	document.getElementById("read-msg-box").style.display = "none";
+	document.getElementById("replyMsg").style.display = "none";
 	document.getElementById("active6").style.backgroundColor = "#27a32d";
 	document.getElementById("active1").style.backgroundColor = "rgba(0,0,0,.9)";
 	document.getElementById("active2").style.backgroundColor = "rgba(0,0,0,.9)";
@@ -131,16 +131,16 @@ function openDeleteBox(){
 	document.getElementById("active7").style.backgroundColor = "rgba(0,0,0,.9)";
 }
 
-function openSettingBox(){
-	document.getElementById("setting-box").style.display="block";
-	document.getElementById("send-box").style.display="none";
-	document.getElementById("inbox-box").style.display="none";
-	document.getElementById("draft-box").style.display="none";
-	document.getElementById("compose-box").style.display="none";
-	document.getElementById("delete-box").style.display="none";
-	document.getElementById("spam-box").style.display="none";
-	document.getElementById("read-msg-box").style.display="none";
-	document.getElementById("replyMsg").style.display="none";
+function openSettingBox() {
+	document.getElementById("setting-box").style.display = "block";
+	document.getElementById("send-box").style.display = "none";
+	document.getElementById("inbox-box").style.display = "none";
+	document.getElementById("draft-box").style.display = "none";
+	document.getElementById("compose-box").style.display = "none";
+	document.getElementById("delete-box").style.display = "none";
+	document.getElementById("spam-box").style.display = "none";
+	document.getElementById("read-msg-box").style.display = "none";
+	document.getElementById("replyMsg").style.display = "none";
 	document.getElementById("active7").style.backgroundColor = "#27a32d";
 	document.getElementById("active1").style.backgroundColor = "rgba(0,0,0,.9)";
 	document.getElementById("active2").style.backgroundColor = "rgba(0,0,0,.9)";
@@ -150,21 +150,21 @@ function openSettingBox(){
 	document.getElementById("active6").style.backgroundColor = "rgba(0,0,0,.9)";
 }
 
-function readMsgBox(){
-	document.getElementById("setting-box").style.display="none";
-	document.getElementById("send-box").style.display="none";
-	document.getElementById("inbox-box").style.display="none";
-	document.getElementById("draft-box").style.display="none";
-	document.getElementById("compose-box").style.display="none";
-	document.getElementById("delete-box").style.display="none";
-	document.getElementById("spam-box").style.display="none";
-	document.getElementById("read-msg-box").style.display="block";
-	document.getElementById("replyMsg").style.display="none";
+function readMsgBox() {
+	document.getElementById("setting-box").style.display = "none";
+	document.getElementById("send-box").style.display = "none";
+	document.getElementById("inbox-box").style.display = "none";
+	document.getElementById("draft-box").style.display = "none";
+	document.getElementById("compose-box").style.display = "none";
+	document.getElementById("delete-box").style.display = "none";
+	document.getElementById("spam-box").style.display = "none";
+	document.getElementById("read-msg-box").style.display = "block";
+	document.getElementById("replyMsg").style.display = "none";
 }
-function replyMsg(){
-	document.getElementById("replyMsg").style.display="block";
-	document.getElementById("reply").value="reply"
+function replyMsg() {
+	document.getElementById("replyMsg").style.display = "block";
+	document.getElementById("reply").value = "reply"
 }
-function bcBtn(){
-	document.getElementById('bc-hidden').style.display="block";
+function bcBtn() {
+	document.getElementById('bc-hidden').style.display = "block";
 }
